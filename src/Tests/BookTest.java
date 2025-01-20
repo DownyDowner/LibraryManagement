8cd289a914735e96e6da04b39f3b9e8bf8853ba7
DownyDowner <bobo.vancauter@live.fr>
2025-01-20 08:38:30 +0100

    Add: adding JUnit

---
+++ src/Tests/BookTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
